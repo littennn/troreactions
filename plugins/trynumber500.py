45120e619a5353c235e906e0f6540a8484280e9c
litten <71162745+littennn@users.noreply.github.com>
2020-11-12 18:42:07 -0500

    Create trynumber500.py

---
+++ plugins/trynumber500.py
@@ -0,0 +1,6 @@
+@client.event
+async def on_message(message):
+    if message.channel.id = '715397952708804661':
+        emoji = '✔️'
+        emoji2 = '🚫'
+        await message.add_reaction(emoji,emoji2)
