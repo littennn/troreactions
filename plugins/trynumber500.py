19758cffc2d5139658f1495444deb8c26234a3b8
litten <71162745+littennn@users.noreply.github.com>
2020-11-12 18:47:53 -0500

    Delete trynumber500.py

--- plugins/trynumber500.py
+++
@@ -1,6 +0,0 @@
-@client.event
-async def on_message(message):
-    if message.channel.id = '715397952708804661':
-        emoji = '✔️'
-        emoji2 = '🚫'
-        await message.add_reaction(emoji,emoji2)
