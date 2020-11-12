91975bd15089850fa6dbb6e885686fc1cf061381
litten <71162745+littennn@users.noreply.github.com>
2020-11-12 18:53:22 -0500

    Update plugin.py

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -5,4 +5,4 @@ async def on_message(message):
     if message.channel.id = '715397952708804661':
         emoji = '✔️'
         emoji2 = '🚫'
-        await message.add_reaction(emoji,emoji2)
+        await message.add_reaction(emoji,)
